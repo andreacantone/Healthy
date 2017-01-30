4de29cf7a1045bec72fd450b80c4f323475b2131
Cantone Andrea Antonio <andreacantone@libero.it>
2017-01-30 11:01:26 +0100

    Questo è il primo invito

---
+++ ViewController1.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController1.swift
+//  MenuLaterale
+//
+//  Created by Cantone Andrea Antonio on 29/01/17.
+//  Copyright © 2017 Cantone Andrea Antonio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController1: UIViewController {
+    
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if revealViewController() != nil {
+            menuButton.target=revealViewController()
+            menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
+            view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
