eb628aa54cceda2e4b0228dee2cd00e21caad22e
Anna Belardo <anna.belardo94@gmail.com>
2017-01-31 14:24:38 +0100

    riassunto profilo fatto (manca il logout e il collegamento alle pagine che non ci sono ancora ) + inizio modifica profilo (da finire il segue)

---
+++ MenuLaterale/ModifyProfile.swift
@@ -0,0 +1,58 @@
+//
+//  ModifyProfile.swift
+//  MenuLaterale
+//
+//  Created by Anna Belardo on 30/01/17.
+//  Copyright © 2017 Cantone Andrea Antonio. All rights reserved.
+//
+
+import UIKit
+
+class ModifyProfile: UIViewController {
+    @IBOutlet var newN : UILabel!
+    @IBOutlet var newS: UILabel!
+    @IBOutlet var newI : UILabel!
+    @IBOutlet var newE : UILabel!
+    @IBOutlet var newB : UILabel!
+    @IBOutlet var newP: UILabel!
+    @IBOutlet var newIm : UIImageView!
+
+    var newName : String = ""
+    var newSurname : String = ""
+    var newICHO : String = ""
+    var newEmail : String = ""
+    var newBirthdate : String = ""
+    var newPassword : String = ""
+    var newImage : String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        newN.text = newName
+        newS.text = newSurname
+        newI.text = newICHO
+        newE.text = newEmail
+        newB.text = newBirthdate
+        newP.text = newPassword
+        newIm.image = UIImage (named: newImage)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //Salvo i nuovi dati del profilo modificati
+    @IBAction func save(_ sender: UIButton){
+        
+        
+    }
+    
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+   
+    
+}
--- ViewController1.swift
+++ ViewController1.swift
@@ -11,8 +11,45 @@ import UIKit
 class ViewController1: UIViewController {
     
     @IBOutlet weak var menuButton: UIBarButtonItem!
-
+    @IBOutlet var LoggedName: UILabel!
+    @IBOutlet var LoggedSurname: UILabel!
+    @IBOutlet var LoggedICHO: UILabel!
+    @IBOutlet var OminoImage: UIImageView!
+    //DA VEDERE se fisso o se deve funzionare il log in
+    var Name = "Anna"
+    var Surname = "Belardo"
+    var ICHO = "12"
+    var Email = "anna.belardo94@gmail.com"
+    var Birthdate = "02/09/1994"
+    var Password = "raffaelegay"
+    
+    
+    
     override func viewDidLoad() {
+        //Label per Nome Profilo
+        LoggedName.layer.borderWidth = 1.0
+        LoggedName.layer.cornerRadius = 7
+        LoggedName.layer.borderColor = UIColor.gray.cgColor
+        LoggedName.text = Name
+        //Label per Cognome Profilo
+        LoggedSurname.layer.borderWidth = 1.0
+        LoggedSurname.layer.cornerRadius = 7
+        LoggedSurname.layer.borderColor = UIColor.gray.cgColor
+        LoggedSurname.text = Surname
+        //Label per ICHO Profilo
+        LoggedICHO.layer.borderWidth = 1.0
+        LoggedICHO.layer.cornerRadius = 7
+        LoggedICHO.layer.borderColor = UIColor.gray.cgColor
+        LoggedICHO.text = ICHO
+        //immagine rotonda
+        OminoImage.layer.borderWidth = 1
+        OminoImage.layer.masksToBounds = false
+        OminoImage.layer.cornerRadius = OminoImage.frame.size.height/2
+        OminoImage.layer.cornerRadius = OminoImage.frame.size.width/2
+        OminoImage.clipsToBounds = true
+        OminoImage.image = UIImage(named: "ominopc")
+        
+        //gesture per swap
         super.viewDidLoad()
         if revealViewController() != nil {
             menuButton.target=revealViewController()
@@ -22,20 +59,24 @@ class ViewController1: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "ModifyProfile"{
+            let destinationController = segue.destination as! ModifyProfile
+            destinationController.newName = Name
+            destinationController.newSurname = Surname
+            destinationController.newICHO = ICHO
+            destinationController.newEmail = Email
+            destinationController.newPassword = Password
+            destinationController.newImage = Image
+            
+        }
     }
-    */
 
 }
